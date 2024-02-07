693bddde84cdaba83995901b6c606d87462570ec
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-02-07 09:28:05 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio7fechas
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Que fecha desea (dd/mm/yyyy) : ");
+            DateTime fecha = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Cuantos dias deseas: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Fecha con los dias sumados: {fecha.AddDays(dias)}");
+            Console.WriteLine($"Fecha con los dias restados: {fecha.AddDays(-dias)}");
+
+        }
+    }
+}
